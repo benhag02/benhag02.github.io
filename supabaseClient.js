f7bdc7eb5554e74cba233deb011dd566b64c92a1
Ben <benhag02@gmail.com>
2026-06-10 13:29:27 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
-const supabaseUrl = 'https://ojnkqqphpsxxjfyrfviy.supabase.co/rest/v1/'
+const supabaseUrl = 'https://ojnkqqphpsxxjfyrfviy.supabase.co'
 const supabaseKey = 'sb_publishable_5nB7dT8aWlWLeElo-mFuWg_ALnMVjhG'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
